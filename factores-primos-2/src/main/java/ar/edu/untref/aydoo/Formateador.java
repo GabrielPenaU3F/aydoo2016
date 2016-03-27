c9907ed045328ceb7e8bef44789cdf93c5744ff5
Gabriel Pena <synlgp@gmail.com>
2016-03-27 17:56:14 -0300

    Quito comportamiento de la interfaz

--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/Formateador.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/Formateador.java
@@ -4,16 +4,23 @@ import java.util.List;
 
 public class Formateador {
 	
-	public static String formatear(String numero, String formato) {
-		String pretty = "--format=pretty";
-        String quiet = "--format=quiet";
-        if (formato.compareToIgnoreCase(pretty) == 0) {
-        	return formatearPretty(numero);
-        } else if (formato.compareToIgnoreCase(quiet) == 0) {
-        	return formatearQuiet(numero);
-        } else {
-        	return "Formato no aceptado. Las opciones posibles son: pretty o quiet.";
-        }
+	public static String formatear(String[] args) {
+		final String pretty = "--format=pretty";
+        final String quiet = "--format=quiet";
+        final String numero = args[0];
+		if(args.length == 1) {
+			return formatearPretty(numero);
+		} else if (args.length == 2) {
+			final String formato = args[1];
+	        if ((formato.compareToIgnoreCase(pretty) == 0)) {
+	        	return formatearPretty(numero);
+	        } else if (formato.compareToIgnoreCase(quiet) == 0) {
+	        	return formatearQuiet(numero);
+	        } else {
+	        	return "Formato no aceptado. Las opciones posibles son: pretty o quiet.";
+	        }
+		}
+		return "";
 	}
 	
 	public static String formatearPretty(String numero) {
--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,13 +1,11 @@
 package ar.edu.untref.aydoo;
 
-import java.util.List;
-import java.util.Scanner;
 
 public class Program 
 {
     public static final void main(String args[])
     {
-    	System.out.print(Formateador.formatear(args[0], args[1]));
+    	System.out.print(Formateador.formatear(args));
     }
     
 }
